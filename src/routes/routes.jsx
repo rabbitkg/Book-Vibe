37f7a8fc01f9a20db1d18abd88393f7b9e48d88f
rabbitkg <rabbitkgbd@gmail.com>
2026-04-06 04:27:52 +0600

    build a navbar.

---
+++ src/routes/routes.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout";
+import HomePage from "../pages/homepage/HomePage";
+import Books from "../pages/books/Books";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout/>,
+    children: [
+      {
+        index: true,
+        element: <HomePage/>
+      },
+      {
+        path: "/books",
+        element: <Books/>
+      }
+    ]
+  },
+  
+]);
